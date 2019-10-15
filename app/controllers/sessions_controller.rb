146cdba6a371a21c2cc691228d91c4bced874f9b
Cameron Scott <camscottgreen@hotmail.com>
2019-10-15 16:37:35 +1000

    Add simple login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        @user = User.find_by_email(params[:email])
+        if @user
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash.now[:error] = "No such user"
+            render 'new'
+        end
+    end
+
+    def destroy
+        session.delete[:user_id]
+    end
+end
